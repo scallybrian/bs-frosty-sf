7244a7808670576c0fde4074901d6f9c16544343
Brian Scally <scallybrian@gmail.com>
2022-08-26 14:48:12 +0100

    fix(challenge1): :zap: Update the macro and model to be more generic

--- frostyfridays/models/challenge_001/challenge_001__populate_table.sql
+++ frostyfridays/models/challenge_001/challenge_001__populate_table.sql
@@ -1,11 +1,14 @@
 {{ config(
-    pre_hook = stage_and_copy(
+    pre_hook = create_stage(
         stage_name = 'challenge_001_stage', 
-        options = 'url = s3://frostyfridaychallenges/challenge_1/', 
-        columns = ['result'], 
-        column_types = ['varchar'],
-        file_type = 'CSV'),
+        options = "url = 's3://frostyfridaychallenges/challenge_1/' file_format = (type = 'CSV', skip_header = 1)", 
+        ),
     materialized = 'table'
 ) }}
 
-select * from {{ this }}
+select 
+    $1,
+    metadata$filename as file_name,
+    metadata$file_row_number as row_number 
+from @challenge_001_stage
+order by 2,3
